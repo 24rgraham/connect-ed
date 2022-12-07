f665e061c83b7b54d112b06d426b84dde63beba7
D-Dursty <devondahlia@gmail.com>
2022-12-07 12:31:06 -0800

    react is working now

---
+++ src/App.js
@@ -0,0 +1,23 @@
+// import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+// import Navbar from "./components/Navbar";
+import React from 'react';
+
+
+
+function App() {
+  return (
+    <div className="app">
+        hello
+      {/* <Router basename="/connect-ed">
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+         
+        </Routes>
+        <h1>Footer</h1>
+      </Router> */}
+    </div>
+  );
+}
+
+export default App;
