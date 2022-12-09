7a233322581946296d0087dba8ea8b39d3f68155
D-Dursty <devondahlia@gmail.com>
2022-12-09 06:50:22 -0800

    new project form, attempted to get things where we want them, distributed components for landing page

---
+++ src/components/LandingComponents/OriginalProjects/index.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+import ProjectCard from '../../ProjectCard'
+
+
+export default function OriginalProjects() {
+    return(
+        <div className='originalProjects'>
+                <h4>Original Projects:</h4>
+            </div>
+    )
+}
--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -2,27 +2,21 @@ import React from 'react'
 import './style.css'
 
 import ProjectCard from '../../components/ProjectCard'
+import InProgress from '../../components/LandingComponents/InProgress'
+import Completed from '../../components/LandingComponents/Completed'
+import Starred from '../../components/LandingComponents/Starred'
+import SavedForLater from '../../components/LandingComponents/SavedForLater'
+import OriginalProjects from '../../components/LandingComponents/OriginalProjects'
 
 // Make a project card component that will be .map() placed in each associated container
 export default function Landing() {
     return(
         <div className='landingContainer'>
-            <div className='inProgress'> 
-                <h4>In Progress:</h4>
-            </div>
-            <div className='completed'>
-                <h4>Completed Projects:</h4>
-                <div><ProjectCard/></div>
-            </div>
-            <div className='starred'>
-                <h4>Starred Projects:</h4>
-            </div>
-            <div className='saved4Later'>
-                <h4>Saved For Later:</h4>
-            </div>
-            <div className='originalProjects'>
-                <h4>Original Projects:</h4>
-            </div>
+            <InProgress/>
+            <Completed/>
+            <Starred/>
+            <SavedForLater/>
+            <OriginalProjects/>
         </div>
     )
 }
