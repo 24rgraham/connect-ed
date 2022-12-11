75d348cf5fe31387bc8443c531dc0ea0bbe07f2b
bryce <brycelmckenzie@gmail.com>
2022-12-11 12:31:03 -0800

    header and nav layout now responsive

--- src/App.js
+++ src/App.js
@@ -1,32 +1,41 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+// import Navbar from "./components/Navbar";
+import Navbarbootstrap from "./components/Navbarbootstrap";
 import React from "react";
-import Header from "./components/Header/";
+import Headerbootstrap from "./components/Headerbootstrap/";
+// import Header from "./components/Header/";
 import Main from "./pages/Main";
-import Login from "./pages/Login";
 import "./App.css";
 
 function App() {
   return (
-    <div className="app">
-      <Router basename="/connect-ed">
-        <div className="pageContainer">
-          <div className="header">
-            <Header />
-          </div>
-          <body>
-            <div className="nav">
-              <Navbar />
-            </div>
-
-            <div className="main">
-              <Main />
-            </div>
-          </body>
-          <h1>Footer</h1>
-        </div>
+    <div className="App">
+      <Router>
+      <Headerbootstrap />
+      <Routes>
+        <Route path="/connect-ed/*" element={<Main />}></Route>
+      </Routes>
       </Router>
+
     </div>
+    // <div className="app">
+    //   <Router basename="/connect-ed">
+    //     <div className="pageContainer">
+    //       <div className="header">
+    //         {/* <Header /> */}
+    //       </div>
+    //         <div className="nav">
+    //           <Navbarbootstrap />
+    //         </div>
+    //       <div className="body">
+    //         <div className="main">
+    //           <Main />
+    //         </div>
+    //       </div>
+    //       <h1>Footer</h1>
+    //     </div>
+    //   </Router>
+    // </div>
   );
 }
 
