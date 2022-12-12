470e29d88e0d4d970ed7781ad79f39aacc57c3d3
D-Dursty <devondahlia@gmail.com>
2022-12-11 18:40:36 -0800

    please

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React, {useEffect,useState} from "react";
 import API from './utils/API'
+import './App.css'
 
-import Navbar from "./components/Navbar";
-import Header from "./components/Header/";
+import Headerbootstrap from './components/Headerbootstrap'
 import Main from "./pages/Main";
 
-import "./App.css";
+
 
 function App() {
   const [userId, setUserId] = useState(0)
   const [userEmail, setUserEmail] = useState("")
   const [isLoggedIn, setIsLoggedIn] = useState(false)
   const [token, setToken] = useState("")
+
+  useEffect(()=>{
+    const storedToken = localStorage.getItem("token")
+    if(storedToken){
+      console.log(storedToken)
+      API.getUserFromToken(storedToken).then(data=>{
+        if(data.user){
+          console.log(data)
+          setToken(storedToken)
+          setIsLoggedIn(true)
+          setUserId(data.user.id)
+          setUserEmail(data.user.email)
+        }
+      })
+    } else {
+      console.log('no stored token')
+    }
+  },[])
+
+  const handleLoginSubmit = userObj=>{
+    API.login(userObj).then(data=>{
+      console.log(data);
+      if(data.token){
+        setUserId(data.user.id)
+        setToken(data.token)
+        setIsLoggedIn(true)
+        setUserEmail(data.user.email)
+        localStorage.setItem("token",data.token)
+      }
+    })
+  }
 
   
   return (
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,23 +1,53 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
+import {useNavigate} from 'react-router-dom'
 
-export default function Login() {
+export default function Login(props) {
+
+  const navigate = useNavigate();
+    useEffect(()=>{
+      if(props.isLoggedIn){
+        navigate("/mypage")
+      }
+    },[props.isLoggedIn])
+
+  const [loginEmail, setLoginEmail] = useState("")
+  const [loginPassword, setLoginPassword] = useState("")
+ 
+  const loginHandle=e=>{
+    e.preventDefault();
+    props.handleLoginSubmit({
+      email:loginEmail, 
+      password:loginPassword
+    })
+  }
+
   return (
-    <form>
-      <div className="">
-        <label for="InputEmail" className="form-label">
-          Email Address
+    <form onSubmit={loginHandle}>
+      <div>
+        <label for="InputEmail" className="form-label" >
+          Email Address 
         </label>
         <input
           className="form-control"
+          type="text"
           id="InputEmail"
           aria-describedby="emailHelp"
+          name="email" 
+          value={loginEmail} 
+          onChange={e=>setLoginEmail(e.target.value)}
         ></input>
       </div>
       <div className="">
         <label for="InputPassword" className="form-label">
           Password
         </label>
-        <input className="form-control" id="InputPassword"></input>
+        <input type="password"
+        className="form-control" 
+        id="InputPassword"
+        name="password"
+        value={loginPassword}
+        onChange={e=>setLoginPassword(e.target.value)}
+        ></input>
       </div>
       <button type="submit" className="btn btn-primary">
         Submit
