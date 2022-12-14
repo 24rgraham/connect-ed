329639d31fa3a546b9b2467d38aa5d3b520cb577
bryce <brycelmckenzie@gmail.com>
2022-12-13 23:29:12 -0800

    search function working

--- src/components/LandingComponents/OriginalProjects/index.js
+++ src/components/LandingComponents/OriginalProjects/index.js
@@ -2,27 +2,29 @@ import React, { useEffect, useState } from "react";
 import API from "../../../utils/API";
 import ProjectCard from "../../ProjectCard";
 
-
-import './style.css'
+import "./style.css";
 
 export default function OgProjects(props) {
-    const [ogProjects,setOgProjects] = useState([])
-    useEffect(() => {
-      const storedToken = localStorage.getItem("token");
-         API.getOgProjects(storedToken).then((data) => {
-               console.log(data);
-               setOgProjects(data)
-           });
-    },[] )
-    return(
-        <>
-            {ogProjects && <div className='inProgress'>
-                <h6>MY Projects:</h6>
-                <div className='statusCards'>
-
-                {ogProjects.map((projectInfo) => (<ProjectCard key={projectInfo.id} projectInfo={projectInfo}/>))}
-                </div>
-            </div>}
-            </>
-    )
+  const [ogProjects, setOgProjects] = useState([]);
+  useEffect(() => {
+    const storedToken = localStorage.getItem("token");
+    API.getOgProjects(storedToken).then((data) => {
+      console.log(data);
+      setOgProjects(data);
+    });
+  }, []);
+  return (
+    <>
+      {ogProjects && (
+        <div className="inProgress">
+          <h6>MY Projects:</h6>
+          <div className="statusCards">
+            {ogProjects.map((projectInfo) => (
+              <ProjectCard key={projectInfo.id} projectInfo={projectInfo} />
+            ))}
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
